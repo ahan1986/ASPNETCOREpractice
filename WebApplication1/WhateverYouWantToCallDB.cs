582261792b828402803026b17a05556743ac0740
Andrew Han <andrew.a.han@gmail.com>
2018-11-07 16:48:11 -0500

    updating the database

--- WebApplication1/WhateverYouWantToCallDB.cs
+++ WebApplication1/WhateverYouWantToCallDB.cs
@@ -1,6 +1,16 @@
-﻿namespace WebApplication1
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1
 {
-    internal class WhateverYouWantToCallDB
+    // this is representing the database
+    public class WhateverYouWantToCallDB : DbContext
     {
+        //constructor
+        public WhateverYouWantToCallDB(DbContextOptions options) 
+            : base(options) {
+
+        }
+        // we have to set it
+        public DbSet<Customers> Customer { get; set; }
     }
 }
