5e7b0282ffb44e54ae94e04f5fb8ddfa698910d6
Andrew Han <andrew.a.han@gmail.com>
2018-11-07 11:57:48 -0500

    adding web1application to get used to razor

---
+++ WebApplication1/Customers.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(10)]
+        public string Name { get; set; }    
+    }
+}
